0c82caf4db0fd8b0db9137e644a71cf4bdc609d8
Mr raju <143610202+majid598@users.noreply.github.com>
2024-06-10 22:55:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:4173",
-      "https://instagram-clone-eight-lovat.vercel.app",
+      "https://instagram-eight-lovat.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
